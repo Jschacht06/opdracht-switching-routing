15e9ab6fac2db6bbf19d7fe4659f07203f1d74a1
root <root@bachelor-vm-12.devbit.be>
2026-03-05 14:30:51 +0000

    completed mqtt sensor script

--- sensors.py
+++ sensors.py
@@ -3,8 +3,28 @@ import paho.mqtt.client as mqtt
 
 
 BROKER = "localhost" #moet denk ik internal docker ip worden
+PORT = 1883
+TOPIC_BATHROOM = "/home/sensors/bathroom"
+TOPIC_BEDROOM = "/home/sensors/bedroom"
 
+mqttc = mqtt.Client()
+mqttc.connect(BROKER, PORT)
+
+while True:
 #Bathroom
+    bath_temp = random.randint(10, 35)
+    bath_humi = random.randint(30, 70)
 
+    msg = {'temperature': bath_temp, 'humidity': bath_humi}
+    msg_string = json.dumps(msg)
+    mqttc.publish(TOPIC_BATHROOM, msg_string)
+    time.sleep(5)
 
 #Bedroom
+    bed_temp = random.randint(10, 35)
+    bed_humi = random.randint(30, 70)
+
+    msg = {'temperature': bed_temp, 'humidity': bed_humi}
+    msg_string = json.dumps(msg)
+    mqttc.publish(TOPIC_BEDROOM, msg_string)
+    time.sleep(5)
