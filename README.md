a2eefe1fc2c9fca5f7b236a1275dbb0eef7d1d6c
Arthur Brassaert <abrassaert@gmail.com>
2026-03-18 11:55:18 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # opdracht-switching-routing
+
+Voorlopige build command voor sensors: ```docker build -t sensors .``` (in terminal in sensors folder).
--- sensors/sensors.py
+++ sensors/sensors.py
@@ -6,25 +6,35 @@ BROKER = "mosquitto" #moet internal docker ip worden
 PORT = 1883
 TOPIC_BATHROOM = "/home/sensors/bathroom"
 TOPIC_BEDROOM = "/home/sensors/bedroom"
+TOPIC_SERVER_ROOM = "/home/sensors/server_room"
 
 mqttc = mqtt.Client()
 mqttc.connect(BROKER, PORT)
 
 while True:
 #Bathroom
-    bath_temp = random.randint(10, 35)
-    bath_humi = random.randint(30, 70)
+    bath_temp = random.randint(270.00, 302.50)
+    bath_humi = random.randint(0.50, 0.85)
 
     msg = {'temperature': bath_temp, 'humidity': bath_humi}
     msg_string = json.dumps(msg)
     mqttc.publish(TOPIC_BATHROOM, msg_string)
-    time.sleep(5)
 
 #Bedroom
-    bed_temp = random.randint(10, 35)
-    bed_humi = random.randint(30, 70)
+    bed_temp = random.randint(282.50, 297.50)
+    bed_humi = random.randint(0.40, 0.55)
 
     msg = {'temperature': bed_temp, 'humidity': bed_humi}
     msg_string = json.dumps(msg)
     mqttc.publish(TOPIC_BEDROOM, msg_string)
+
+#Server room
+    server_temp = random.randint(290.00, 320.00)
+    server_humi = random.randint(0.30, 0.45)
+
+    msg = {'temperature': server_temp, 'humidity': server_humi}
+    msg_string = json.dumps(msg)
+    mqttc.publish(TOPIC_SERVER_ROOM, msg_string)
+    
+#Global wait
     time.sleep(5)
