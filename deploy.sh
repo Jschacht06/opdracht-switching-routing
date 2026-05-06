ca473eb62203169442d60d5b837b8136ddf0a735
Juha Schacht <juha.schacht@gmail.com>
2026-05-06 17:33:25 +0200

    fixed double datafolder

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+cd "$(dirname "$0")"
+
+echo "Stopping containers before cleaning InfluxDB..."
+docker compose down
+
+echo "Deleting InfluxDB data and config..."
+rm -rf ./influxdb/data
+rm -rf ./influxdb/config
+
+echo "Recreating InfluxDB folders..."
+mkdir -p ./influxdb/data
+mkdir -p ./influxdb/config
+
 echo "Pulling latest Docker images..."
 docker compose pull
 
